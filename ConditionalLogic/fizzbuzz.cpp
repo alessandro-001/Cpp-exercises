72f556f2a6e9406881503329a656b2b1aaad9b60
alessandro-001 <alessandrofrondini@gmail.com>
2023-10-26 15:25:49 +0100

    fizzbuzz challenge

--- ConditionalLogic/fizzbuzz.cpp
+++ ConditionalLogic/fizzbuzz.cpp
@@ -2,6 +2,18 @@
 Write a fizzbuzz.cpp program that outputs numbers from 1 to 100.
 But for multiples of 3, print Fizz instead of the number and for the multiples of 5, print Buzz. 
 For numbers which are multiples of both 3 and 5 print, FizzBuzz.
+ex:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+...
 */
 
 #include <iostream>
