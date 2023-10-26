eef01ce1030e756375270c9db06c7bb0184b8905
alessandro-001 <alessandrofrondini@gmail.com>
2023-10-26 17:21:50 +0100

    moved exercise

---
+++ Loops/fizzbuzz.cpp
@@ -0,0 +1,32 @@
+/**
+Write a fizzbuzz.cpp program that outputs numbers from 1 to 100.
+But for multiples of 3, print Fizz instead of the number and for the multiples of 5, print Buzz. 
+For numbers which are multiples of both 3 and 5 print, FizzBuzz.
+ex:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+...
+*/
+
+#include <iostream>
+
+int main() {
+
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      std::cout << "Fizz! \n";
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz! \n";
+    } else {
+      std::cout << i << "\n";
+    }
+  }
+}
